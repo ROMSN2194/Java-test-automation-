70d7e84cecaad61d0bea515383dfbb36a685f685
ROMSN2194 <lebedovichroman1@i.ua>
2017-01-22 20:46:42 +0200

    Update GmailTest.java

--- GmailTest/src/test/java/com/epam/tests/GmailTest.java
+++ GmailTest/src/test/java/com/epam/tests/GmailTest.java
@@ -3,11 +3,7 @@ package com.epam.tests;
 import static org.testng.AssertJUnit.*;
 
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,21 +18,16 @@ public class GmailTest {
   
   @Test(priority=0)
   public void loginTest() {
-	 
-	 assertEquals("Gmail",user.getDriver().getTitle());
-	 user.login(email, password);
-	 	  }
+	  user.login(email, password);
+	  assertEquals("Gmail",user.getDriver().getTitle());
+  }
   
   @Test(priority=1)
   public void  composeTest() throws InterruptedException{
 	  int count=user.getDraftsCount();
-	  System.out.println(count);
-	  System.out.println(System.getenv("JAVA_HOME"));
+	 
 	  user.compose("RomanLebedovych@gmail.com", "subject556","text");
 	  user.openInDraft();
 	  assertEquals(count+1,user.getDraftsCount());  
-	  user.sendMessage();	  
-	  
   }
-  
 }
